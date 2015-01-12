f7047274e1575820048954c4e4a80b6e0d9842d2
JohnWeng <hweng1953@gmail.com>
2015-01-12 09:43:33 -0500

    say someone made changed and committed
    say someone made changed and committed

--- gitTutorial/src/gitTutorial/Class1.java
+++ gitTutorial/src/gitTutorial/Class1.java
@@ -4,7 +4,7 @@ public class Class1 {
 // haha
 	public void function(){
 		System.out.println("new push on 1-12-2015");
-			
+		System.out.println("say someone made changed and committed");	
 	}
 	
 }
