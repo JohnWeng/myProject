72b380eac92335dbc24f0a25cf2aed89e8baa3d2
JohnWeng <hweng1953@gmail.com>
2015-01-12 09:51:34 -0500

    Update Class1.java

--- gitTutorial/src/gitTutorial/Class1.java
+++ gitTutorial/src/gitTutorial/Class1.java
@@ -3,7 +3,7 @@ package gitTutorial;
 public class Class1 {
 // haha
 	public void function(){
-		System.out.println("new push on 1-12-20105");
+		System.out.println("update");
 		System.out.println("say someone made changed and committed");	
 	}
 	
